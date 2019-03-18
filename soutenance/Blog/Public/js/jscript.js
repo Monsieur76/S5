30729bbef8f54f4d1d511d392c8eb9edcec7d594
Monsieur76 <46344706+Monsieur76@users.noreply.github.com>
2019-03-18 14:51:27 +0100

    Delete jscript.js

--- soutenance/Blog/Public/js/jscript.js
+++
@@ -1,8 +0,0 @@
- $(document ).ready(function() {
-     $('.fn_modify_modal').click(function (e) {
-         if (confirm("Ete vous sur de vouloir confirmer ?")) {
-         } else {
-             e.preventDefault();
-         }
-     })
-    });
