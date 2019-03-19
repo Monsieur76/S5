1ad400513f70bc754f9bb43404461fd6f10d9bbf
Monsieur76 <46344706+Monsieur76@users.noreply.github.com>
2019-03-19 10:19:54 +0100

    Update index.php

--- soutenance/Blog/Public/index.php
+++ soutenance/Blog/Public/index.php
@@ -2,6 +2,7 @@
 session_start();
 require '../src/DAO/Database.php';
 require '../Config/autoloader.php';
+ini_set('display_errors','off');
 App\Config\Autoloader::register();
 $index = new App\Config\Router;
 $index->run();
