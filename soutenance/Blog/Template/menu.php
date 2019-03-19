486b5399b8f7ca5529f99f4a9ca6c717962639f4
monsieur76 <gaetanhenry76@gmail.com>
2019-03-19 17:11:36 +0100

    composer

--- soutenance/Blog/Template/menu.php
+++ soutenance/Blog/Template/menu.php
@@ -1,6 +1,8 @@
 <?php
 $control = new \App\src\Controller\BackController;
 $route = $_GET['route'];
+$routeAdmin = ['Administration', 'Validation_de_Suppression', 'Validation_de_commentaire', 'Confirmation_Ajout_Article',
+    'confirmation_modification_article', 'Confirmation_Ajout_Utilisateur', 'supprimer_utilisateur', 'Refuser_Commentaire'];
 if ($control->isUserConnected() === true) {
     ?>
     <div class="container-fluid" style="background-color: rgba(255, 255, 255,0);">
@@ -13,22 +15,21 @@ if ($control->isUserConnected() === true) {
                     } ?>
                             href="?route=Accueil"><i class="fas fa-home"> Accueil</i></a></li>
                 <li class="nav-item col-lg-3" style="list-style-type: none;"><a
-                            id="menu" <?php if ($route === 'liste_des_Articles'||
-                        $route === 'Afficher_un_Article'||
+                            id="menu" <?php if ($route === 'liste_des_Articles' ||
+                        $route === 'Afficher_un_Article' ||
                         $route === 'Validation_de_Commentaire') {
                         echo 'class="nav-link active"';
                     } ?>
                             href='?route=liste_des_Articles'><i class="fas fa-list-ul"> Liste des articles</i></a></li>
                 <li class="nav-item col-lg-3" style="list-style-type: none;"><a
-                            id="menu" <?php if ($route === 'Administration'||
-                        $route === 'Validation_de_Suppression' ||$route === 'Validation_de_commentaire'
-                    ||$route === 'Confirmation_Ajout_Article'||
-                        $route === 'confirmation_modification_article'
-                        ||$route === 'Confirmation_Ajout_Utilisateur'||
-                        $route === 'supprimer_utilisateur'||
-                        $route === 'Refuser_Commentaire') {
-                        echo 'class="nav-link active"';
-                    } ?>
+                            id="menu"
+                        <?php
+                        foreach ($routeAdmin as $donne) {
+                            if ($route === $donne) {
+                                echo 'class="nav-link active"';
+                            }
+                        }
+                        ?>
                             href='?route=Administration'><i class="fas fa-toolbox"> Administration</i></a>
                 </li>
                 <li class="nav-item col-lg-3" style="list-style-type: none;"><a
